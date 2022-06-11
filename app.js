e31e14bf20ac5cae6019c7d781dfc2631b877027
Nazim Filzer <nazimfilzer@gmail.com>
2022-06-11 20:05:36 +0530

    Level 4 Auth(bcrypt)

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5=require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,33 +37,39 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
 
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets")
-        }
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets")
+            }
+        });
     });
 
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password == password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if (result == true) {
+                        res.render("secrets");
+                    }
+                })
             }
         }
     });
