c882c6edc8281864b71e8d5995a204f03dab187c
Nazim Filzer <nazimfilzer@gmail.com>
2022-06-12 07:26:45 +0530

    Level6 OAuth

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
+
+//Problems- Asks Google Signin Everytime we log in (Ig session not saved)
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -26,23 +30,59 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 
 app.get("/", (req, res) => {
     res.render("home");
 })
 
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect secrets.
+        res.redirect('/secrets');
+    });
 app.get("/login", (req, res) => {
     res.render("login");
 })
@@ -52,20 +92,52 @@ app.get("/register", (req, res) => {
 })
 
 app.get("/secrets", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("secrets")
-    } else {
-        res.redirect("/login")
-    }
+    User.find({"secret": {$ne:null}},(err,foundUser)=>{
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                console.log(foundUser);
+                res.render("secrets",{userWithSecrets:foundUser})
+            }
+        }
+    })
 })
 
-app.get("/logout",(req,res)=>{
-    req.logout((err)=>{
-        if(!err){
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", (req, res) => {
+    const submittedSecret = req.body.secret;
+
+    // console.log(req.user); { id: '62a4b327b76bda3dbb9497ad', username: 'nazim@gmail.com' }
+
+    User.findById(req.user.id, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(() => {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
+app.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (!err) {
             res.redirect("/");
         }
     });
-})
+});
 
 app.post("/register", (req, res) => {
 
